a5e602a302d9dd75f889877cd67382ba148ef1dc
fasih uddin <uddin24fasih@yahoo.com>
2020-03-27 21:16:31 -0400

    first commit....

---
+++ src/PersonInfo/Mian.java
@@ -0,0 +1,40 @@
+package PersonInfo;
+
+public class Mian {
+public static void main(String[] args) {
+String name="fasih";
+int age=30;
+double annualPay=100000;
+ 
+System.out.println("my name is "+name);
+System.out.println("i am "+age+" years old");
+System.out.println("i hope to earn $"+annualPay);
+
+
+
+
+System.out.println("name Fasih");
+System.out.println("37-05 90 st jackson heights NY11372");
+System.out.println("3475984762");
+System.out.println("computer science");
+System.out.println("uddin24fasih@yahoo.com");
+
+
+//star pattern
+int n=5;
+for(int i=0;i<n;i++) {
+	for(int j=0;j<i;j++) {
+		System.out.print("*");
+	}
+	System.out.println();
+}
+
+
+}
+
+
+
+
+
+
+}
